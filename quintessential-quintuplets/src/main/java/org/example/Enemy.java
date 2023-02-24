606e18d60d354a902a8c456bab76d955d900d236
Victor Vasconcellos <victor.amaim@gmail.com>
2023-02-24 09:20:02 -0800

    Created the initial java project

---
+++ quintessential-quintuplets/src/main/java/org/example/Enemy.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Enemy extends Sprite{
+    public Enemy (Color color, int size, PVector position, int health, int speed){
+        super(color, size, position, health, speed);
+    }
+}
