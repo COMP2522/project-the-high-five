87fb35a6f8a264c1eb504d5e540e5dfca309ad00
Victor Vasconcellos <victor.amaim@gmail.com>
2023-02-24 10:14:12 -0800

    Created the initial Enemy.java and EnemyManager.java classes.

--- quintessential-quintuplets/src/main/java/org/example/Enemy.java
+++ quintessential-quintuplets/src/main/java/org/example/Enemy.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import processing.core.PVector;
+import java.awt.*;
+
 public class Enemy extends Sprite{
     public Enemy (Color color, int size, PVector position, int health, int speed){
         super(color, size, position, health, speed);
