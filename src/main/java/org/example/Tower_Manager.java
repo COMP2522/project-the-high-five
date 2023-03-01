0877e1d4c6992c3f1e6d3e2f8b6f47045644b053
GursidhSandhu <gsandhu168@my.bcit.ca>
2023-03-01 15:15:50 -0800

    testing to see if this branch works

---
+++ src/main/java/org/example/Tower_Manager.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.ArrayList;
+
+/**
+ * This class manages the tower class
+ * @author gursidhsandhu
+ */
+public abstract class Tower_Manager {
+
+  private ArrayList<Tower> towers;
+}
