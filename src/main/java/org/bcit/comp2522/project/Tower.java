25bad6683bcaed5fab3294f068ec7216408835cf
GursidhSandhu <gsandhu168@my.bcit.ca>
2023-03-01 17:26:15 -0800

    testing to see if this branch works

--- src/main/java/org/bcit/comp2522/project/Tower.java
+++ src/main/java/org/bcit/comp2522/project/Tower.java
@@ -5,9 +5,20 @@ package org.bcit.comp2522.project;
  * Contains attributes and methods each tower
  * object should have
  * @author gursidhsandhu
+ * @version 2023
  */
-public class Tower extends Tower_Manager{
+public class Tower extends Sprite{
 // methods unique to this class
+
+  /**
+   * Constructor for tower class
+   * @param xpos
+   * @param ypos
+   * @param window
+   */
+  public Tower(float xpos, float ypos, Window window){
+    super(xpos, ypos, window);
+  }
   private int attackRate;
   private int range;
 
--- src/main/java/org/bcit/comp2522/project/TowerManager.java
+++ src/main/java/org/bcit/comp2522/project/TowerManager.java
@@ -5,9 +5,16 @@ import java.util.ArrayList;
 /**
  * This class manages the tower class
  * @author gursidhsandhu
+ * @version 2023
  */
-public abstract class Tower_Manager {
+public abstract class TowerManager extends Tower {
 
   // list of tower objects
   private ArrayList<Tower> towers;
+
+  int numbOfTowers;
+
+  public TowerManager(float xpos, float ypos, Window window) {
+    super(xpos, ypos, window);
+  }
 }
