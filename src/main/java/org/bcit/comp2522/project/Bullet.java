f896f465d5341ca70dc24a9a405d77e3cd3938b8
arickor <aricor10945@gmail.com>
2023-03-20 14:27:31 -0700

    made Bullet and BulletManager

---
+++ src/main/java/org/bcit/comp2522/project/Bullet.java
@@ -0,0 +1,21 @@
+package org.bcit.comp2522.project;
+
+/**
+ * Bullet.java
+ * Represents the bullets in the game.
+ *
+ * @author Aric Or
+ * @version 1.0
+ */
+public class Bullet extends Sprite {
+
+    public Bullet(float xpos, float ypos, Window window) {
+        super(xpos, ypos, window);
+    }
+
+    public void draw() {
+//        window.fill(255, 0, 0);
+//        window.ellipse(xpos, ypos, 10, 10);
+    }
+
+}
---
+++ src/main/java/org/bcit/comp2522/project/BulletManager.java
@@ -0,0 +1,16 @@
+package org.bcit.comp2522.project;
+
+import java.util.ArrayList;
+
+/**
+ * BulletManager.java
+ * Manages the bullets in the game.
+ *
+ * @author Aric Or
+ * @version 1.0
+ */
+public class BulletManager {
+
+    private ArrayList<Bullet> bullets;
+
+}
