958e1e2e9d40ee48ddd4b6166fb27f1bde01fdb6
GursidhSandhu <gsandhu168@my.bcit.ca>
2023-03-26 01:42:52 -0700

    created a design for towers and finished draw method. every tower has same appearance and appears properly on window

--- src/main/java/org/bcit/comp2522/project/TowerManager.java
+++ src/main/java/org/bcit/comp2522/project/TowerManager.java
@@ -6,8 +6,13 @@ import java.util.ArrayList;
  * This class manages the tower class
  * @author gursidhsandhu
  */
-public abstract class Tower_Manager {
+public class TowerManager {
 
   // list of tower objects
   private ArrayList<Tower> towers;
+
+  public TowerManager (){
+    this.towers = new ArrayList<Tower>();
+  }
+
 }
