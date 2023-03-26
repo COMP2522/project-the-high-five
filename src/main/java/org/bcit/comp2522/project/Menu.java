4af56569c4ef566f2edf18131f919394b504579e
Cheryl Lau <clau136@my.bcit.ca>
2023-03-26 16:35:38 -0700

    Display menu buttons Start Game and Load Game

--- src/main/java/org/bcit/comp2522/project/Menu.java
+++ src/main/java/org/bcit/comp2522/project/Menu.java
@@ -1,12 +1,17 @@
 package org.bcit.comp2522.project;
 
 import processing.core.PApplet;
+import processing.core.PFont;
 import processing.core.PImage;
+
+import java.sql.SQLOutput;
 
 public class Menu {
 
     private PApplet parent;
     private PImage backgroundImage;
+
+
 
     public Menu(PApplet parent){
         this.parent = parent;
@@ -16,7 +21,35 @@ public class Menu {
     }
 
     public void display() {
-        parent.image(backgroundImage, 0, 0, parent.width, parent.height);
+
+        PApplet p = parent;
+
+        PFont gameFont;
+        int horizontalPos = p.width/2;
+        int startPos = p.height * 5 / 14;
+        int loadPos = p.height * 8 / 14;
+        int buttonWidth = 400;
+        int buttonHeight = 100;
+
+        p.image(backgroundImage, 0, 0, p.width, p.height);
+        p.rectMode(p.CENTER);
+        p.fill(0,0,0);
+        p.rect(horizontalPos, startPos, buttonWidth,buttonHeight);
+        p.rect(horizontalPos, loadPos, buttonWidth,buttonHeight);
+
+        String dataPath = p.sketchPath("fonts");
+        gameFont = p.createFont("" + dataPath + "/Righteous-Regular.ttf", 400);
+
+        p.textFont(gameFont);
+        p.fill(255);
+        p.textSize(40);
+        p.textAlign(parent.CENTER, parent.CENTER);
+        p.text("START GAME",horizontalPos,startPos);
+        p.text("LOAD GAME",horizontalPos,loadPos);
+
+
+
+
     }
 
 }
