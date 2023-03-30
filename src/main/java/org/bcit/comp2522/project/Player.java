e1692d534a7413f84496e0a83625a6bcce9e1e05
Cheryl Lau <clau136@my.bcit.ca>
2023-03-30 15:56:06 -0700

    Added getter and setters for player

--- src/main/java/org/bcit/comp2522/project/Player.java
+++ src/main/java/org/bcit/comp2522/project/Player.java
@@ -1,29 +1,61 @@
 package org.bcit.comp2522.project;
 
 public class Player {
-private static Player player;
+    private static Player player;
+    int currentLevel;
+    int currentScore;
+    int coins;
 
-  int currentLevel;
-  int currentScore;
-  int coins;
+  public int getCurrentLevel() {
+    return currentLevel;
+  }
+
+  public void setCurrentLevel(int currentLevel) {
+    this.currentLevel = currentLevel;
+  }
+
+  public int getCurrentScore() {
+    return currentScore;
+  }
+
+  public void setCurrentScore(int currentScore) {
+    this.currentScore = currentScore;
+  }
+
+  public int getCoins() {
+    return coins;
+  }
+
+  public void setCoins(int coins) {
+    this.coins = coins;
+  }
+
+  public int getHealth() {
+    return health;
+  }
+
+  public void setHealth(int health) {
+    this.health = health;
+  }
+
   int health;
 
 
-  private Player(){
-    currentLevel = 0;
-    currentScore = 0;
-    coins = 0;
-  };
+    private Player() {
+        currentLevel = 0;
+        currentScore = 0;
+        coins = 0;
+    }
 
-  public static Player getInstance(){
-    if(player == null) {
-      player = new Player();
+
+    public static Player getInstance() {
+        if (player == null) {
+            player = new Player();
+
+        }
+        return player;
 
     }
-    return player;
-
-  }
-
 
 
 }
