766f46796d1338b4be60a425e4eeb1e812dcf173
Cheryl Lau <clau136@my.bcit.ca>
2023-03-30 16:30:38 -0700

    Update Player, when level changes, stats reset

--- src/main/java/org/bcit/comp2522/project/Player.java
+++ src/main/java/org/bcit/comp2522/project/Player.java
@@ -2,50 +2,52 @@ package org.bcit.comp2522.project;
 
 public class Player {
     private static Player player;
-    int currentLevel;
-    int currentScore;
-    int coins;
-
-  public int getCurrentLevel() {
-    return currentLevel;
-  }
-
-  public void setCurrentLevel(int currentLevel) {
-    this.currentLevel = currentLevel;
-  }
-
-  public int getCurrentScore() {
-    return currentScore;
-  }
-
-  public void setCurrentScore(int currentScore) {
-    this.currentScore = currentScore;
-  }
-
-  public int getCoins() {
-    return coins;
-  }
-
-  public void setCoins(int coins) {
-    this.coins = coins;
-  }
-
-  public int getHealth() {
-    return health;
-  }
-
-  public void setHealth(int health) {
-    this.health = health;
-  }
-
-  int health;
-
+    private int currentLevel;
+    private int currentScore;
+    private int coins;
+   private int health;
 
     private Player() {
         currentLevel = 0;
-        currentScore = 0;
-        coins = 0;
+        currentScore = 1000;
+        coins = 100;
+        health = 10;
     }
+
+    public int getCurrentLevel() {
+        return currentLevel;
+    }
+
+    public void setCurrentLevel(int currentLevel) {
+        this.currentLevel = currentLevel;
+    }
+
+    public int getCurrentScore() {
+        return currentScore;
+    }
+
+    public void setCurrentScore(int currentScore) {
+        this.currentScore = currentScore;
+    }
+
+    public int getCoins() {
+        return coins;
+    }
+
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+
+
 
 
     public static Player getInstance() {
@@ -54,6 +56,12 @@ public class Player {
 
         }
         return player;
+
+    }
+
+    public static void resetStats() {
+        player.setCoins(0);
+        player.setCoins(10);
 
     }
 
