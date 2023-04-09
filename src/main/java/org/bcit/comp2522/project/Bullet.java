94aeea397a16bf3f99e010ef383181985f0175e4
arickor <113308237+arickor@users.noreply.github.com>
2023-04-09 14:28:26 -0700

    Cleaned up the Bullet class.

--- src/main/java/org/bcit/comp2522/project/Bullet.java
+++ src/main/java/org/bcit/comp2522/project/Bullet.java
@@ -9,24 +9,25 @@ package org.bcit.comp2522.project;
  */
 public class Bullet extends Sprite {
 
-  public int getSpeed() {
-    return speed;
-  }
-
+  /* The speed of the bullet. */
   private int speed;
-
+  /* The size of the bullet. */
   private int size;
-
+  /* The angle of the bullet. */
   private float angle;
-
-  public int getDamage() {
-    return damage;
-  }
-
+  /* The damage of the bullet. */
   private int damage = 1;
-
+  /* Whether the bullet has hit a target. */
   private boolean isHit = false;
 
+  /**
+   * Constructs a Bullet object.
+   *
+   * @param xpos   The x position of the bullet.
+   * @param ypos   The y position of the bullet.
+   * @param window The window the bullet is in.
+   * @param angle  The angle of the bullet.
+   */
   public Bullet(float xpos, float ypos, Window window, float angle) {
     super(xpos, ypos, window);
     this.speed = 10;
@@ -34,24 +35,51 @@ public class Bullet extends Sprite {
     this.angle = angle;
   }
 
+  /**
+   * Returns the damage of the bullet.
+   *
+   * @return the damage of the bullet.
+   */
+  public int getDamage() {
+    return damage;
+  }
+
+  /**
+   * Returns the size of the bullet.
+   * @return the size of the bullet.
+   */
   public int getSize() {
     return size;
   }
 
+  /**
+   * Moves the bullet.
+   */
   public void move() {
     setXpos(getXpos() + (float) (speed * Math.cos(angle)));
     setYpos(getYpos() + (float) (speed * Math.sin(angle)));
   }
 
+  /**
+   * Draws the bullet.
+   */
   public void draw() {
     window.fill(0, 0, 255);
     window.circle(getXpos(), getYpos(), 10);
   }
 
+  /**
+   * Returns whether the bullet has hit a target.
+   * @return whether the bullet has hit a target.
+   */
   public boolean getIsHit() {
     return isHit;
   }
 
+  /**
+   * Sets whether the bullet has hit a target.
+   * @param isHit whether the bullet has hit a target.
+   */
   public void setIsHit(boolean isHit) {
     this.isHit = isHit;
   }
