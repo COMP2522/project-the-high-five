99fcae5c9551f797977b5aae785dedcef2b7c7c1
victoramaim <vcarvalhodiasamaimva@my.bcit.ca>
2023-04-08 21:21:05 -0700

    Created javadocs in Enemy, EnemyManager, Level, LevelManager, and each individual level class.

--- src/main/java/org/bcit/comp2522/project/Level_3.java
+++ src/main/java/org/bcit/comp2522/project/Level_3.java
@@ -1,19 +1,30 @@
 package org.bcit.comp2522.project;
 
-import processing.core.PImage;
-
-import java.util.ArrayList;
-
+/**
+ Level_3 class is a subclass of Level that represents the third level of the game.
+ It initializes the level's components, draws the game objects on the screen,
+ and checks when it is time to spawn a new enemy or to end the level.
+ @author quintessentialquintuplets
+ @version 1.0
+ */
 public class Level_3 extends Level {
 
+  /**
+   * Constructs a Level_3 object.
+   * @param window the window that the level is drawn on
+   */
   public Level_3(Window window) {
     super(window);
     init();
   }
 
+  /**
+   Initializes the level by resetting the level, adding corners to the path, and setting the path on the tile map.
+   It also updates the timer and checks if it's time to spawn a new enemy.
+   */
   public void init() {
     levelreset();
-    //levelManager = new LevelManager(window, 5);
+
     getPath().clearCorner();
     getPath().addCorner(40, 528);
     getPath().addCorner(232, 528);
@@ -32,6 +43,10 @@ public class Level_3 extends Level {
     getTileMap().setPath();
   }
 
+  /**
+   Draws the game objects on the screen, updates the timer and checks if it's time to spawn a new enemy.
+   It also checks if the game is over, and if all enemies are defeated.
+   */
   public void draw() {
     if (Player.getHealth() <= 0) {
       getWindow().setStage(3);
@@ -78,8 +93,5 @@ public class Level_3 extends Level {
 
     getBulletManager().update();
     getEnemyManager().update(getBulletManager());
-    //getWindow().grid.draw();
-    //tileMap.checkMap();
-
   }
 }
