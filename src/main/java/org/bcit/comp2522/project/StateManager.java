ee0c93c2fcfbaa08170bb1eb7c136fea4439f24d
arickor <113308237+arickor@users.noreply.github.com>
2023-04-10 00:12:52 -0700

    checkstyle

--- src/main/java/org/bcit/comp2522/project/StateManager.java
+++ src/main/java/org/bcit/comp2522/project/StateManager.java
@@ -4,54 +4,57 @@ package org.bcit.comp2522.project;
 import org.json.JSONObject;
 
 /**
- * Manages the game state by reading and writing it to the database. It has reference to the current GameState and
+ * Manages the game state by reading and writing it to the database.
+ * It has reference to the current GameState and
  * DatabaseHanlder objects, as well as a Window object.
  */
 public class StateManager {
 
-    private final GameState gamestate;
-    private final DatabaseHandler dbh;
+  private final GameState gamestate;
+  private final DatabaseHandler dbh;
 
-    private final Window window;
+  private final Window window;
 
-    private GameState gs;
-
-
-    /**
-     * Constructor for StateManager class. Initializes the GameState, DatabaseHandler and Window objects.
-     * @param window The Window object for accessing the LevelManager and updating the game display.
-     * @param lm The LevelManager object for manging the game levels.
-     */
-    StateManager(Window window, LevelManager lm) {
-        this.window = window;
-        gamestate = new GameState(Player.getInstance(), window);
-        dbh = new DatabaseHandler();
+  private GameState gs;
 
 
-    }
+  /**
+   * Constructor for StateManager class. Initializes the GameState,
+   * DatabaseHandler and Window objects.
+   *
+   * @param window The Window object for accessing the LevelManager and updating the game display.
+   * @param lm     The LevelManager object for manging the game levels.
+   */
+  StateManager(Window window, LevelManager lm) {
+    this.window = window;
+    gamestate = new GameState(Player.getInstance(), window);
+    dbh = new DatabaseHandler();
 
 
-    /**
-     * Writes the given GameState to the database.
-     * @param jsonObject the GameState object to be written to the database.
-     */
-    public void push(JSONObject jsonObject) {
-        dbh.writeGameState(jsonObject);
-    }
-
-    /**
-     * Reads the player's game state from the database and updates the Player and LevelManager objects with the data.
-     */
-    public void pull(){
-        GameState gs = dbh.getGameState(window);
-        Player.setCurrentScore(gs.getCurrentScore());
-        Player.setCoins(gs.getCoins());
-        Player.setHealth(gs.getPlayerHealth());
-        LevelManager.setCurrentLevel(gs.getCurrentLevel());
-        EnemyManager.setEnemiesKilled(gs.getEnemiesKilled());
-    }
+  }
 
 
+  /**
+   * Writes the given GameState to the database.
+   *
+   * @param jsonObject the GameState object to be written to the database.
+   */
+  public void push(JSONObject jsonObject) {
+    dbh.writeGameState(jsonObject);
+  }
+
+  /**
+   * Reads the player's game state from the database and updates the Player
+   * and LevelManager objects with the data.
+   */
+  public void pull() {
+    GameState gs = dbh.getGameState(window);
+    Player.setCurrentScore(gs.getCurrentScore());
+    Player.setCoins(gs.getCoins());
+    Player.setHealth(gs.getPlayerHealth());
+    LevelManager.setCurrentLevel(gs.getCurrentLevel());
+    EnemyManager.setEnemiesKilled(gs.getEnemiesKilled());
+  }
 
 
 }
